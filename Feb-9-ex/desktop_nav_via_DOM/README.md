76e801eb8eac6447231ff8cee6d6907f95fe58b1
Frankie  Molina <frankiemolina@Frankies-MBP.lan>
2023-02-17 01:03:38 -0800

    add desktop nav attempt without looping

---
+++ Feb-9-ex/desktop_nav_via_DOM/README.md
@@ -0,0 +1,10 @@
+I need to populate a header on the browser window using the DOM and Javascript (NO HTML ALLOWED). The header should contain the following items:
+
+Home, Portfolio, About, Contact &
+Privacy Policy
+
+Once the header is in the DOM, I need to style it using the CSS Flexbox module and other CSS properties. 
+
+Furthermore, the `Home` item must be proportiantely larger than the remaining items of the header. 
+
+The final constraint is that my header must be identical to the image titled `replicateThisHeader` in the `images` directory of this repo. 
--- Feb-9-ex/desktop_nav_via_DOM/main.js
+++ Feb-9-ex/desktop_nav_via_DOM/main.js
@@ -1,3 +1,31 @@
 const app = document.getElementById('app');
-const header = document.createElement('header');
+
+const navMain = document.createElement('nav');
+
+app.appendChild(navMain);
+
+const createNavList = document.createElement('ul');
+
+navMain.appendChild(createNavList);
+
+const createHome = document.createElement('li')
+
+createNavList.appendChild(createHome);
+
+createHome.textContent = 'Home';
+
+const createPortfolio = document.createElement('li')
+
+createNavList.appendChild(createPortfolio);
+
+createPortfolio.textContent = 'Portfolio';
+
+
+
+
+
+
+
+
+
 
